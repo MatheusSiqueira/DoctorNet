adbac8f35d418af492161fe8a4c4012043d0f7f2
matheus siqueira braz <siqueirabraz@hotmail.com>
2021-08-23 19:54:59 -0300

    Criando Entidades

---
+++ Doctor.Domain/Model/Cadastro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Doctor.Domain.Model
+{
+    public class Cadastro
+    {
+        public long CadastroId { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Senha { get; set; }
+        public int NumeroEndereco { get; set; }
+        public long EnderecoId { get; set; }
+        public virtual Endereco Endereco { get; set; }
+    }
+}
---
+++ Doctor.Domain/Model/Endereco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doctor.Domain.Model
+{
+    public class Endereco
+    {
+        public long EnderecoId { get; set; }
+        public string Nome { get; set; }
+    }
+}
